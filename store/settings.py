fc26c0f59bad92ce9320ae1fd3ef0f0d1e1ffcd4
Vladislav <scroynel@gmail.com>
2023-03-02 18:20:23 +0100

    DATABASE connect settings

--- store/settings.py
+++ store/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'drfproj', 
+        'USER': 'postgres',
+        'PASSWORD': '4628600',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
